4f412ae2ac4e2290341d111885607a77ce146a80
Dawit Yifter <dawityifter@gmail.com>
2020-09-20 16:02:20 -0500

    Added petstore-api.yml

---
+++ src/test/java/com/dawityifter/trail/ApiFirstDemo/ApiFirstDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dawityifter.trail.ApiFirstDemo;
+
+import com.dawityifter.trail.apifirst.ApiFirstDemoApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(classes = ApiFirstDemoApplication.class)
+class ApiFirstDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
